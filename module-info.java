bcf38035bcc24f584a632f1ae577fb15f8b2b35b
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2022-05-19 21:46:13 +0200

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.lab4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.lab4 to javafx.fxml;
-    exports com.example.lab4;
-}
